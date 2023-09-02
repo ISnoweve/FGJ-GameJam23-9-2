8743a24a1597a870bf04651f74848b9de6880aa5
小仔 <71455438+chris58530@users.noreply.github.com>
2023-09-02 17:31:19 +0800

    camera

---
+++ G828FGJ/Assets/Script/System/CameraFollow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Transform player;
+
+    // void Awake()
+    // {
+    //     player = GameObject.FindGameObjectWithTag("Player").gameObject.transform;
+    // }
+
+
+    // void LateUpdate()
+    // {
+    //     transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+    // }
+    // void FollowPlayer()
+    // {
+
+    // }
+
+}
